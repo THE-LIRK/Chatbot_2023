e143bd6368fe6803911bc604f1bd7aea9fcce576
THE-LIRK <121623903+THE-LIRK@users.noreply.github.com>
2023-05-16 16:06:12 +0200

    Update final.py

--- final.py
+++ final.py
@@ -15,14 +15,14 @@ from sklearn.model_selection import train_test_split
 
 # Définir la couleur de fond en rose
 # Affichage de l'interface utilisateur
-st.markdown("<h1 style='text-align: center; color: blue;'>Chatbot</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: red;'>Chatbot</h1>", unsafe_allow_html=True)
 st.markdown(
     """
     <style>
     .stApp {
         background-image:url('https://w.wallhaven.cc/full/39/wallhaven-398lo3.png');
         background-size: cover;
-        color: blue ;
+        color: red ;
     }
     </style>
     """,
